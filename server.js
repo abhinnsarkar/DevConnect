f069a2ec02f36561f21ca6c08faa7fc5247559b1
Abhinn Sarkar <abhinnsarkar@Abhinns-MacBook-Pro.local>
2023-03-13 17:05:00 -0700

    Getting ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,14 +9,21 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // define routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/users", require("./routes/api/users"));
 
-// const PORT = process.env.PORT || 4000;
-const PORT = 4000;
+// serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
